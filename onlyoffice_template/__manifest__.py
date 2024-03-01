389e912aa165bf2f9c64b9ae79403c6315d63b57
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-03-01 16:32:09 +0300

    feat: add template module

---
+++ onlyoffice_template/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ONLYOFFICE Templates",
+
+    'summary': "Automate form creation with inserting fields from Odoo in templates.",
+
+    'description': "The ONLYOFFICE connector allows users to edit and collaborate on office files within Odoo Documents using ONLYOFFICE Docs. You can work with text documents, spreadsheets, and presentations, co-author documents in real time using two co-editing modes (Fast and Strict), Track Changes, comments, and built-in chat.",
+
+    'author': "ONLYOFFICE",
+    'website': "https://www.onlyoffice.com",
+
+    'category': 'Productivity',
+    'version': '2.0.0',
+
+    'depends': ['base', 'onlyoffice_odoo'],
+
+    "external_dependencies": {"python": ["pyjwt"]},
+
+    # always loaded
+    'data': [
+        'security/onlyoffice_template_security.xml',
+        'security/ir.model.access.csv',
+        'views/onlyoffice_menu_views.xml'
+    ],
+
+    'license': 'LGPL-3',
+    'support': 'support@onlyoffice.com',
+
+    'images': [ "static/description/main_screenshot.png", "static/description/icon.png"],
+
+    'installable': True,
+    'application': True,
+
+    'assets': {
+        'web.assets_backend': [
+            'onlyoffice_template/static/src/css/*',
+            'onlyoffice_template/static/src/views/**/*',
+        ],
+    },
+}
---
+++ onlyoffice_template/static/src/views/kanban/onlyoffice_kanban_view.js
@@ -0,0 +1,12 @@
+/** @odoo-module */
+import { registry } from "@web/core/registry";
+import { kanbanView } from "@web/views/kanban/kanban_view";
+import { OnlyofficeKanbanRenderer } from "./onlyoffice_kanban_renderer";
+
+export const onlyofficeKanbanView = {
+  ...kanbanView,
+  Renderer: OnlyofficeKanbanRenderer,
+  buttonTemplate: "onlyoffice_template.OnlyofficeKanbanController.Buttons",
+};
+
+registry.category("views").add("onlyoffice_kanban", onlyofficeKanbanView);
