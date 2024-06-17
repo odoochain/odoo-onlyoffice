7240ac64045cd36f2ce93f85820a4e675b2945ff
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-06-17 23:42:39 +0300

    feat: cache models, field metadata filtering, optimization

--- onlyoffice_odoo_templates/models/onlyoffice_odoo_templates.py
+++ onlyoffice_odoo_templates/models/onlyoffice_odoo_templates.py
@@ -62,12 +62,20 @@ class OnlyOfficeTemplate(models.Model):
         cached_models = {}
 
         def process_model(model_name):
+            if self.is_system_model(model_name):
+                return None
             if model_name in processed_models:
-                return
+                return None
+
             processed_models.add(model_name)
 
-            description = self.env["ir.model"].search_read([["model", "=", model_name]], ["name"])
-            fields = self.env[model_name].fields_get()
+            model = self.env["ir.model"].search([("model", "=", model_name)], limit=1)
+            if not model:
+                processed_models.discard(model_name)
+                return None
+            description = model.name
+
+            fields = self.env[model_name].fields_get([], attributes=("name", "type", "string", "relation"))
 
             field_list = []
             for field_name, field_props in fields.items():
@@ -87,12 +95,16 @@ class OnlyOfficeTemplate(models.Model):
                 if field_type in ["one2many", "many2many", "many2one"]:
                     related_model = field_props["relation"]
                     if cached_models.get(related_model):
-                        field_dict["related_model"] = copy.deepcopy(cached_models[related_model])
-                        field_dict["related_model"]["name"] = field_name
+                        field_dict["related_model"] = {
+                            "name": field_name,
+                            "description": cached_models[related_model]["description"],
+                            "fields": cached_models[related_model]["fields"],
+                        }
                     else:
                         if field_type == "many2one":
-                            related_description = self.env["ir.model"].search_read([["model", "=", related_model]], ["name"])
-                            related_fields = self.env[related_model].fields_get()
+                            related_description = self.env["ir.model"].search([("model", "=", related_model)], limit=1)
+                            related_description = related_description.name
+                            related_fields = self.env[related_model].fields_get([], attributes=("name", "type", "string"))
 
                             related_field_list = []
                             for (related_field_name, related_field_props) in related_fields.items():
@@ -106,7 +118,7 @@ class OnlyOfficeTemplate(models.Model):
                                 related_field_list.append(related_field_dict)
                             related_model_info = {
                                 "name": field_name,
-                                "description": related_description[0]["name"],
+                                "description": related_description,
                                 "fields": related_field_list,
                             }
                             field_dict["related_model"] = related_model_info
@@ -121,7 +133,7 @@ class OnlyOfficeTemplate(models.Model):
 
             model_info = {
                 "name": model_name,
-                "description": description[0]["name"],
+                "description": description,
                 "fields": field_list,
             }
 
@@ -131,6 +143,29 @@ class OnlyOfficeTemplate(models.Model):
         models_info = process_model(model_name)
         data = json.dumps(models_info, ensure_ascii=False)
         return data
+
+    def is_system_model(self, model_name):
+        system_models = {
+            "ir.attachment",  # Attachments
+            "ir.config_parameter",  # Configuration parameters
+            "ir.cron",  # Scheduled tasks
+            "ir.filters",  # Filters
+            "ir.mail_server",  # Mail server
+            "ir.rule",  # Access rules
+            "ir.sequence",  # Sequences
+            "ir.translation",  # Translations
+            "res.groups",  # User groups
+        }
+
+        if model_name in system_models:
+            return True
+
+        system_models_prefixes = ["ir.model.", "ir.module.", "ir.actions", "base.language", "mail.", "discuss."]
+        for prefix in system_models_prefixes:
+            if model_name.startswith(prefix):
+                return True
+
+        return False
 
     def is_system_field(self, field_name):
         system_fields = [
@@ -170,15 +205,15 @@ class OnlyOfficeTemplate(models.Model):
             "edi_error_count",
             "edi_error_message",
             "extract_error_message",
-            "message_has_error",
-            "message_has_error_counter",
-            "message_has_sms_error",
+
+            # Other
+            "self",
         ]
 
         if field_name in system_fields:
             return True
 
-        system_fields_prefixes = ["in_groups_", "sel_groups_"]
+        system_fields_prefixes = ["in_groups_", "sel_groups_", "message_", "activity_", "website_"]
         for prefix in system_fields_prefixes:
             if field_name.startswith(prefix):
                 return True
