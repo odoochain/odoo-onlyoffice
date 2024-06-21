99bc43f007ff8824af7c470e894cfab853a2ac5e
Sergey Linnik <sergey.linnik@onlyoffice.com>
2024-06-21 13:56:07 +0300

    pdf filling

--- onlyoffice_odoo_documents/static/src/models/documents_inspector_onlyoffice.js
+++ onlyoffice_odoo_documents/static/src/models/documents_inspector_onlyoffice.js
@@ -12,15 +12,16 @@ import { useService } from "@web/core/utils/hooks";
 
 const oo_editable_formats = [
     "docx",
-    "pdf",
     "xlsx",
     "pptx",
+    "pdf",
 ]
 
 const oo_viewable_formats = [
     "djvu",
     "doc",
     "docm",
+    "docxf",
     "dot",
     "dotm",
     "dotx",
@@ -32,7 +33,6 @@ const oo_viewable_formats = [
     "odt",
     "ott",
     "oxps",
-    "pdf",
     "rtf",
     "txt",
     "xps",
