58c9b5e6cc7c8d1337805c07ecc2c0078d258e55
Sergey Linnik <sergey.linnik@onlyoffice.com>
2024-07-23 12:58:49 +0300

    templates: 1.0.2

--- CHANGELOG templates.md
+++ CHANGELOG templates.md
@@ -1,6 +1,6 @@
 # Change Log
 
-## 1.0.0
+## 1.0.2
 ## Added
 - new templates module
 - creating and editing templates in editor
--- onlyoffice_odoo_templates/__manifest__.py
+++ onlyoffice_odoo_templates/__manifest__.py
@@ -10,7 +10,7 @@
     'website': "https://www.onlyoffice.com",
 
     'category': 'Productivity',
-    'version': '1.0.0',
+    'version': '1.0.2',
 
     'depends': ['base', 'onlyoffice_odoo'],
 
